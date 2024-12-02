110f8ccc3782b6f1ffdffd05d4a50dc607c9c425
Marcio Rafael <131469066+marciorafaelb@users.noreply.github.com>
2024-12-02 13:41:19 +0000

    1 atividade

---
+++ atv_decisao.py
@@ -0,0 +1,10 @@
+#nao esquece de dar tab!!!! mto importante
+#1 numero1 = int(input("Digite um numero: "))
+#  numero2 = int(input("Digite outro numero: "))
+#
+#  if numero1 > numero2:
+#    print(f" {numero1}")
+#  else:
+#    print(f" {numero2}")
+
+       
