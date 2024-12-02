02d5e2555ce9ed1318bed38df9893da32f95220a
Marcio Rafael <131469066+marciorafaelb@users.noreply.github.com>
2024-12-02 12:57:02 +0000

    4 atividades

--- atv.py
+++ atv.py
@@ -1,1 +1,19 @@
-print('Hello World!')
+#1 print('Hello World!') 
+
+#2 numero = int(input("Digite um numero: "))
+#  print("O numero informado foi", numero)
+
+#3 numero1 = int(input("Digite um numero: "))
+#  numero2 = int(input("Digite outro numero: "))
+#  soma = numero1 + numero2
+#  print("A soma dos numeros é:", soma)
+
+#4 nota1 = float(input("Digite a primeira nota bimestral: "))
+#  nota2 = float(input("Digite a segunda nota bimestral: "))
+#  nota3 = float(input("Digite a terceira nota bimestral: "))
+#  nota4 = float(input("Digite a quarta nota bimestral: "))
+#  media = (nota1 + nota2 + nota3 + nota4) / 4
+#  print("A media do aluno é:", media)
+
+
+
